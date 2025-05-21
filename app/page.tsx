79c756619deb602dbaea2858729662d1e440bd7b
Juan Linares <juanlinares821@csu.fullerton.edu>
2025-05-20 21:15:18 -0700

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <section className="py-24">
+      <div className="container">
+        <h1 className="text-3x1 font-bold">Movie Night</h1>
+      </div>
+    </section>
+  )
+}
