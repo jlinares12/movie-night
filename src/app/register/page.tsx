1c20ba0f18486ea573a5a8d3d5fd56325b119d44
Juan Linares <juanlinares821@csu.fullerton.edu>
2025-05-30 13:31:55 -0700

    Created the sign up form to register a new account

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -2,10 +2,28 @@
 import { useState } from 'react';
 
 export default function RegistrationPage() {
-    const url = 'localhost:8000/api/register';
-    const [newUser, setNewUser] = useState({username: '', email: '', password: '' })
-    function createUser() {
-        console.log("user")
+
+    const url = 'http://localhost:8000/api/register';
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [isPending, setIsPending] = useState(false);
+
+    const createUser = (e: React.FormEvent) => {
+        e.preventDefault();
+
+        const user = {username, email, password}
+
+        setIsPending(true);
+
+        fetch(url, {
+            method: "POST",
+            headers: { "Content-Type": "application/json"},
+            body: JSON.stringify(user)
+        }).then(() => {
+            console.log('user added');
+            setIsPending(false);
+        })
     }
 
     return (
@@ -14,15 +32,34 @@ export default function RegistrationPage() {
                 <span>Sign Up</span>
                 <div>
                     <label htmlFor="username">Username:</label>
-                    <input id="username" placeholder=""></input>
+                    <input
+                    id="username"
+                    type='text'
+                    required
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}/>
+
                     <label htmlFor="email">Email:</label>
-                    <input id="email" type="email"></input>
+                    <input
+                    id="email"
+                    type="email"
+                    required
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}/>
+
                     <label htmlFor="passwrd">Password:</label>
-                    <input id="passwrd"></input>
+                    <input
+                    id="passwrd"
+                    type="password"
+                    required
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}/>
+
                     <label htmlFor="cpasswrd">Confirm Password:</label>
-                    <input id="cpasswrd"></input>
+                    <input id="cpasswrd"/>
                 </div>
-                <input type="submit"></input>
+                {!isPending && <button type='submit'>Sign Up</button>}
+                {isPending && <button disabled>Creating Account...</button>}
             </form>
         </>
     )
