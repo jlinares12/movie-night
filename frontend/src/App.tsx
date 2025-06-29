103ac2e66aafc565f09de3f94493d7bba93d637d
Juan Linares <juanlinares821@csu.fullerton.edu>
2025-06-29 15:34:22 -0700

    Made my react and flask app talk so that now my react component will be rendered using my flask router

---
+++ frontend/src/App.tsx
@@ -0,0 +1,32 @@
+import axios from 'axios';
+import {useState, useEffect} from 'react'
+
+function App() {
+  const [data, setData] = useState({"tech" : []});
+  const fetchAPI = async () => {
+    const response = await axios.get("http://localhost:5000/data");
+    console.log(response.data);
+    setData(response.data)
+  };
+
+  useEffect(() => {
+    fetchAPI()
+  }, [])
+
+  return (
+    <div>
+      <h1>Movie Night</h1>
+      <h3>This project is going to use the following tech stack:</h3>
+      {(typeof data.tech === 'undefined') ? (
+        <p>Loading</p>
+      ): (
+        data.tech.map((tech, i) => (
+          <p key={i}>{tech}</p>
+        ))
+      )
+      }
+    </div>
+  )
+}
+
+export default App
