3a8f8dbbc24f0c79b8e3cb81b1fd4d9cb9fe65d0
Juan Linares <juanlinares821@csu.fullerton.edu>
2025-07-04 00:52:55 -0700

    Added routing funcitonality with react router and also updated how the clerk forms look

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,64 @@
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { StrictMode } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { ClerkProvider } from "@clerk/clerk-react";
 import Layout from "./pages/Layout";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Techstack from "./pages/Techstack";
+import NotFound from "./pages/NotFound";
+import Register from "./pages/Signup";
 import "./index.css"
 
-const root = document.getElementById("root")!;
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
-ReactDOM.createRoot(root).render(
-    <BrowserRouter>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="login" element={<Login />} />
-          <Route path="tech-stack" element={<Techstack />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Publishable Key')
+}
+
+const ROUTER = createBrowserRouter([
+  {
+    element: <Layout/>,
+    children: [
+      {path:"/", element: <Home/>},
+      {path:"/login/*", element: <Login/>},
+      {path:"/register/*", element: <Register/>},
+      {path:"/tech-stack", element: <Techstack/>},
+    ]
+  },
+  {path:"*", element: <NotFound/>}
+]);
+
+const ROOT = document.getElementById("root")!;
+
+ReactDOM.createRoot(ROOT).render(
+    <StrictMode>
+      <ClerkProvider
+      publishableKey={PUBLISHABLE_KEY}
+      afterSignOutUrl='/'
+      appearance={{
+        variables: {
+          colorBackground: '#0C1A0D',       // Dark green background
+          colorText: 'white',               // White text
+          colorPrimary: '#40D952',          // Bright green primary color
+          colorInputBackground: '#1A2E1B',  // Slightly lighter input background
+          colorInputText: 'white',          // Input text color           // Rounded corners
+          fontFamily: 'Inter, sans-serif',  // Modern font
+          fontSize: '16px',
+        },
+        elements: {
+          socialButtonsBlockButton__google: {
+          color: 'white',
+          borderColor: '#40D952'
+          },
+          rootBox: {
+            border: '1px solid #40D952',
+            borderRadius: '10px'
+          }
+        }
+      }}
+      >
+        <RouterProvider router={ROUTER}/>
+      </ClerkProvider>
+    </StrictMode>
   );
