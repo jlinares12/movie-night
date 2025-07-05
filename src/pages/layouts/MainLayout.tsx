e4d08c19218b64e032253e1b054df073274710e7
Juan Linares <juanlinares821@csu.fullerton.edu>
2025-07-05 11:07:47 -0700

    Updated the main layout to include the sidebar component

--- src/pages/layouts/MainLayout.tsx
+++ src/pages/layouts/MainLayout.tsx
@@ -1,11 +1,15 @@
 import { Outlet } from "react-router-dom";
 import Header from "../../components/Header";
+import SideBar from "../../components/SideBar";
 
 export default function MainLayout() {
     return (
-        <div className="h-screen bg-[var(--bk-color)]">
+        <div className="h-screen bg-[var(--bk-color)] text-white flex flex-col">
             <Header/>
-            <Outlet/>
+            <div className="flex h-full">
+                <SideBar/>
+                <Outlet/>
+            </div>
         </div>
     )
 }
