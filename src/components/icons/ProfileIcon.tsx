ed0365cf3e2fc9ee345d3c107b3aa018290d5c7a
Juan Linares <juanlinares821@csu.fullerton.edu>
2025-07-05 11:08:15 -0700

    Created a profile icon

---
+++ src/components/icons/ProfileIcon.tsx
@@ -0,0 +1,19 @@
+interface ProfileIconProps {
+    color:string;
+    className:string;
+}
+
+export default function ProfileIcon({color, className}: ProfileIconProps) {
+    return(
+        <svg
+            fill="none"
+            className={className}
+            aria-hidden="true"
+            role="img"
+            preserveAspectRatio="none"
+            viewBox="4 1 40 46">
+            <path d="M24 21a10 10 0 1 1 10-10 10 10 0 0 1-10 10Zm0-16a6 6 0 1 0 6 6 6 6 0 0 0-6-6ZM42 47H6a2 2 0 0 1-2-2v-6a16 16 0 0 1 16-16h8a16 16 0 0 1 16 16v6a2 2 0 0 1-2 2ZM8 43h32v-4a12 12 0 0 0-12-12h-8A12 12 0 0 0 8 39Z"
+            fill={color}/>
+        </svg>
+    )
+}
