c864478af5ca7400d5fda472f8601663861ccdcf
Juan Linares <juanlinares821@csu.fullerton.edu>
2025-07-06 20:43:48 -0700

    removed the sigoutbutton component and made the sidebar button control the signout pathway

--- src/components/SignoutButton.tsx
+++
@@ -1,9 +0,0 @@
-import { useClerk } from "@clerk/clerk-react"
-
-export default function SignOutButton() {
-  const { signOut } = useClerk()
-
-  return (
-    <button onClick={() => signOut({ redirectUrl: '/login' })}>Log out</button>
-  )
-}
