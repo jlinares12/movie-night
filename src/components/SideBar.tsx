cb68dbfb25687943365841c67d0c9be647f56a8d
Juan Linares <juanlinares821@csu.fullerton.edu>
2025-07-06 20:44:31 -0700

    Created the base sidebar, need to add functionality to allow for the sidebar to collapse

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,16 +1,31 @@
 import { Link } from "react-router-dom"
-import SignOutButton from "./SignoutButton"
 import ProfileIcon from "./icons/ProfileIcon"
 import NavLink from "./NavLink"
+import { useClerk } from "@clerk/clerk-react"
+import CalendarIcon from "./icons/CalendarIcon"
+import LogoutIcon from "./icons/LogoutIcon"
+import CollapseIcon from "./icons/CollapseIcon"
 
 export default function SideBar() {
+    const { signOut } = useClerk()
+
     return (
-        <div className="relative top-0 left-0 z-40 w-[175px] h-full flex flex-col justify-between p-7 gap-7">
-            <Link to={"/profile"}>
-                <NavLink children={<ProfileIcon color="#40D952" className=""/>} label="Profile"/>
-            </Link>
+        <div id="nav" className="relative top-0 left-0 z-40 w-[175px] h-full flex flex-col justify-between p-8 gap-7">
+            <div className="flex flex-col gap-12">
+                <Link to={"/profile"}>
+                    <NavLink icon={(className) => <ProfileIcon color="#40D952" className={className}/> } label="Profile"/>
+                </Link>
+                <Link to={"/#"}>
+                    <NavLink icon={(className) => <CalendarIcon color="#40D952" className={className}/> } label="Calendar"/>
+                </Link>
+                <button onClick={() => signOut({ redirectUrl: '/login' })}>
+                    <NavLink icon={(className) => <LogoutIcon color="#40D952" className={className}/> } label="Log out"/>
+                </button>
+            </div>
             <div>
-                <SignOutButton/>
+                <button>
+                    <NavLink icon={(className) => <CollapseIcon color="#40D952" className={className}/> } label="Collapse"/>
+                </button>
             </div>
         </div>
     )
