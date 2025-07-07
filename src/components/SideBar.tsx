12b166f271cc6f2e2f8489a8d8232c0e91544477
Juan Linares <juanlinares821@csu.fullerton.edu>
2025-07-07 15:02:49 -0700

    added a conditional render for the expand/collapse component

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -6,6 +6,7 @@ import CalendarIcon from "./icons/CalendarIcon"
 import LogoutIcon from "./icons/LogoutIcon"
 import CollapseIcon from "./icons/CollapseIcon"
 import { useState } from "react"
+import ExpandIcon from "./icons/ExpandIcon"
 
 export default function SideBar() {
     const { signOut } = useClerk()
@@ -30,29 +31,36 @@ export default function SideBar() {
             >
                 <Link to={"/profile"}>
                     <NavLink
-                        icon={(className) => <ProfileIcon color="#40D952" className={className}/>} 
+                        icon={(className) => <ProfileIcon color="#40D952" className={className}/>}
                         label={isCollapsed ? "" : "Profile"}
                     />
                 </Link>
                 <Link to={"/#"}>
                     <NavLink
-                        icon={(className) => <CalendarIcon color="#40D952" className={className}/>} 
+                        icon={(className) => <CalendarIcon color="#40D952" className={className}/>}
                         label={isCollapsed ? "" : "Calendar"}
                     />
                 </Link>
                 <button onClick={() => signOut({ redirectUrl: '/login' })}>
                     <NavLink
-                        icon={(className) => <LogoutIcon color="#40D952" className={className}/>} 
+                        icon={(className) => <LogoutIcon color="#40D952" className={className}/>}
                         label={isCollapsed ? "" : "Log out"}
                     />
                 </button>
             </div>
             <div>
                 <button onClick={collapse}>
-                    <NavLink
-                        icon={(className) => <CollapseIcon color="#40D952" className={className}/>} 
+                    {
+                        isCollapsed ?
+                        <NavLink
+                        icon={(className) => <ExpandIcon color="#40D952" className={className}/>}
+                        label=""
+                    />:
+                        <NavLink
+                        icon={(className) => <CollapseIcon color="#40D952" className={className}/>}
                         label={isCollapsed ? "" : "Collapse"}
                     />
+                    }
                 </button>
             </div>
         </div>
