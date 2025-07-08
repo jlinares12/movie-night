3a237b9dd8376dd276c39a70688b06f9c820a7d8
Juan Linares <juanlinares821@csu.fullerton.edu>
2025-07-08 10:34:35 -0700

    Created the basic layout for the home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,17 @@
 export default function Home(){
   return (
-    <>
-      <h1 className="underline">Home</h1>
-    </>
+    <div className="grid grid-flow-row grid-row-2 gap-12">
+      <div className="w-[910px] h-[390px] bg-[var(--primary-color)]">
 
+      </div>
+      <div className="grid grid-cols-2 gap-12">
+        <div className="w-[425px] h-[350px] bg-[var(--primary-color)]">
+
+        </div>
+        <div className="w-[425px] h-[350px] bg-[var(--primary-color)]">
+
+        </div>
+      </div>
+    </div>
   )
 };
