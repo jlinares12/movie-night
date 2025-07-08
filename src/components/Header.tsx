f1ac81cf4966eac7ecdeed9d4f4b0c7fd92c4b58
Juan Linares <juanlinares821@csu.fullerton.edu>
2025-07-08 10:34:04 -0700

    Added a link and replaced the movie clapper to a navlink component that redirects you to the home page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,20 @@
 import { useUser } from "@clerk/clerk-react"
+import { Link } from "react-router-dom";
 import MovieClapper from "./icons/MovieClapper"
 import NotificationIcon from "./icons/NotificationIcon"
+import NavLink from "./NavLink";
 
 export default function Header() {
     const {user} = useUser();
     return (
         <header className="h-[65px] w-full flex flex-row items-center justify-between pr-8 pl-8">
             <div className="flex items-center justify-center gap-[15px]">
-                <div className="w-[30px] h-[30px]">
-                    <MovieClapper color="#40D952" className="w-full h-full"/>
-                </div>
-                <p className="text-[var(--text-color)]">Movie Nights</p>
+                <Link to="/">
+                    <NavLink
+                        icon={(className) => <MovieClapper color="#40D952" className={className}/>}
+                        label="Movie Nights"
+                    />
+                </Link>
             </div>
             <h1 className="text-[var(--text-color)]">{user?.username}</h1>
             <div className="w-[30px] h-[30px]">
