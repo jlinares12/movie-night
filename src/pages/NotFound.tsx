fb9e698f4784e5ef9b5e63db0ddbb3a7cfa43f3b
Juan Linares <juanlinares821@csu.fullerton.edu>
2025-07-08 17:43:34 -0700

    Updated the layout of the not found page and changed the text explaining what happend to users

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -3,18 +3,18 @@ import NotFoundIcon from "../components/icons/NotFoundIcon";
 
 export default function NotFound() {
     return (
-        <div className="h-screen bg-[var(--bk-color)] flex flex-col items-center justify-center">
-            <div className="flex justify-between items-center text-[var(--primary-color)] h-[384px]">
+        <div className="h-screen bg-[var(--bk-color)] flex flex-col items-center justify-center gap-5">
+            <div className="flex items-center text-[var(--primary-color)] h-[384px]">
                 <h1 className="text-[384px]">4</h1>
-                <div className="w-96 h-96">
+                <div className="w-[384px] h-[384px]">
                     <NotFoundIcon color="#40D952" className=""/>
                 </div>
                 <h1 className="text-[384px]">4</h1>
             </div>
             <div className="text-[var(--text-color)] flex flex-col justify-between gap-5">
-                <p className="text-4xl">The page you entered cannot be found</p>
-                <Link to={"/"} className="text-xl self-center p-2 outline outline-[var(--primary-color)] rounded-md">
-                    <button>Return Home</button>
+                <p className="text-lg">The page you are looking for might have been removed, had its name changed, or is temporarily unavailable</p>
+                <Link to={"/"} className="text-xl self-center p-2 outline outline-[1px] outline-offset-2 outline-[var(--primary-color)] rounded-md text-lg">
+                    Return Home
                 </Link>
             </div>
         </div>
