89d41ff7c4c3a47b4f2bd49613f55bfbf095e16f
Juan Linares <juanlinares821@csu.fullerton.edu>
2025-07-09 20:46:38 -0700

    Fixed layout and added the MovieGroups component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
+import MovieGroups from "../components/MovieGroups";
+
 export default function Home(){
   return (
     <div className="grid grid-flow-row grid-row-2 gap-12">
       <div className="w-[910px] h-[390px] bg-[var(--primary-color)]">
-
+        <MovieGroups/>
       </div>
-      <div className="grid grid-cols-2 gap-12">
+      <div className="flex justify-between">
         <div className="w-[425px] h-[350px] bg-[var(--primary-color)]">
 
         </div>
