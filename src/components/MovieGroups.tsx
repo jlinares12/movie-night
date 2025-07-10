041cecc2d0fb40f0d8f0057b4d32a94b797eaadb
Juan Linares <juanlinares821@csu.fullerton.edu>
2025-07-09 22:21:51 -0700

    Created a moviegroups component that will show all of the groups you are a part of and will let you navigate to that groups url

---
+++ src/components/MovieGroups.tsx
@@ -0,0 +1,9 @@
+export default function MovieGroups() {
+    return (
+        <div className="flex flex-col items-center justify-center">
+            <div>
+                <h1>Your Groups</h1>
+            </div>
+        </div>
+    )
+}
