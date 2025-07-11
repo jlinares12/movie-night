022454c659c11715cf4e8bef292f9485254eecd1
Juan Linares <juanlinares821@csu.fullerton.edu>
2025-07-11 14:24:31 -0700

    Changed the layout and added the three buttons

--- src/components/GroupLink.tsx
+++ src/components/GroupLink.tsx
@@ -1,3 +1,7 @@
+import OutlinedButton from "./buttons/OutlinedButton";
+import FilledButton from "./buttons/FilledButton";
+import WarningButton from "./buttons/DangerButton";
+
 interface Props {
     key: number;
     name: string;
@@ -7,15 +11,24 @@ interface Props {
 
 export default function GroupLink({name, user_count, date}:Props) {
     return (
-        <div className="flex flex-col p-4">
-            <div className="cl-group-name text-2xl text-[var(--name-color)]">
-                <h1>{name}</h1>
+        <div className="grid grid-cols-3 items-center pb-8 pt-8">
+            <div className="flex">
+                <div className="cl-group-name text-2xl text-[var(--name-color)]">
+                    <h1>{name}</h1>
+                </div>
             </div>
-            <div className="text-sm text-[var(--member-color)]">
-                <p>{user_count} members</p>
+            <div>
+                <div className="text-sm text-[var(--member-color)]">
+                    <p>{user_count} members</p>
+                </div>
+                <div className="text-sm text-[var(--text-color)]">
+                    <p>Date: {date}</p>
+                </div>
             </div>
-            <div className="text-sm text-[var(--text-color)]">
-                <p>Date: {date}</p>
+            <div className="flex gap-2">
+                <OutlinedButton label="Nominate Movie"/>
+                <FilledButton label="Open"/>
+                <WarningButton label="Leave Group"/>
             </div>
         </div>
     )
