7334d6c1bc7eaabb4fe64c39c16693380da517dd
Juan Linares <juanlinares821@csu.fullerton.edu>
2025-07-11 16:18:56 -0700

    Created a create group and join group component

---
+++ src/components/JoinGroup.tsx
@@ -0,0 +1,13 @@
+import FilledButton from "./buttons/FilledButton";
+
+export default function JoinGroup() {
+    return (
+        <div className="flex gap-2">
+            <input required
+                className="w-full border border-[var(--primary-gray)] rounded-[10px] p-4 text-sm bg-transparent focus:border-[var(--primary-color)]"
+                placeholder="Enter a group code"
+                type="text" />
+            <FilledButton label="Join"/>
+        </div>
+    )
+}
