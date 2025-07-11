9c23ed11ae3aab780264207c83130b96a9aff682
Juan Linares <juanlinares821@csu.fullerton.edu>
2025-07-10 17:01:51 -0700

    Made the moviegroups component call the api and use the groups to show the names as a list

--- src/components/MovieGroups.tsx
+++ src/components/MovieGroups.tsx
@@ -1,8 +1,34 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 export default function MovieGroups() {
+    const [data, setData] = useState([])
+    const fetchAPI = async () => {
+    const response = await axios.get("api/groups");
+    setData(response.data);
+  }
+
+  useEffect(() => {
+    fetchAPI();
+  },[])
+
     return (
         <div className="flex flex-col items-center justify-center">
             <div>
                 <h1>Your Groups</h1>
+            </div>
+            <div className="w-full justify-self-start">
+                {(typeof data === 'undefined') ? (
+                <p>Loading</p>
+                ): (
+                <ul className="list-decimal">
+                    {data.map((group, i) => (
+                    <li key={i} className="">
+                        {group["name"]}
+                    </li>
+                    ))}
+                </ul>
+                )}
             </div>
         </div>
     )
