4976bb65734cefc4f0e46bb0c697f8bae93ae528
Juan Linares <juanlinares821@csu.fullerton.edu>
2025-07-11 14:25:04 -0700

    Changed the title of the component to live outside of the div

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,10 @@ import MovieGroups from "../components/MovieGroups";
 export default function Home(){
   return (
     <div className="grid grid-flow-row grid-row-2 gap-12">
-      <div className="w-[910px] border border-[var(--primary-color)] rounded-[10px] p-8">
+      <div className="text-5xl text-[var(--primary-color)] font-extrabold">
+          <h1>Your Groups</h1>
+      </div>
+      <div className="border border-[var(--primary-color)] rounded-[10px] p-8">
         <MovieGroups/>
       </div>
       <div className="flex justify-between">
