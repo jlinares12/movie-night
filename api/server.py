0c20fb7257e0548c5353fa80c8e355bc94f9ba9e
Juan Linares <juanlinares821@csu.fullerton.edu>
2025-07-10 21:54:09 -0700

    Made the server return more info about the groups

--- api/server.py
+++ api/server.py
@@ -12,12 +12,22 @@ def data():
 def groups():
     return [
   {
+    "id": "1",
     "name": "PipiPoopoo",
-    "id": "1234124124"
+    "user_count": "12",
+    "date": "August 12, 2025"
   },
   {
+    "id": "2",
     "name": "Carmenita Boys",
-    "id": "5345132425"
+    "user_count": "3",
+    "date": "July 23, 2025"
+  },
+  {
+    "id": "3",
+    "name": "Rialto House",
+    "user_count": "7",
+    "date": "August 23, 2025"
   }
 ]
 
