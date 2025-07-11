1373800f82041cbdd821201201fc0cba726ff16e
Juan Linares <juanlinares821@csu.fullerton.edu>
2025-07-11 16:20:19 -0700

    Added some styling and join gorup and create group components

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,30 @@
+import CreateGroup from "../components/CreateGroup";
+import JoinGroup from "../components/JoinGroup";
 import MovieGroups from "../components/MovieGroups";
 
 export default function Home(){
   return (
     <div className="grid grid-flow-row grid-row-2 gap-12">
-      <div className="text-5xl text-[var(--primary-color)] font-extrabold">
-          <h1>Your Groups</h1>
-      </div>
-      <div className="border border-[var(--primary-color)] rounded-[10px] p-8">
-        <MovieGroups/>
-      </div>
-      <div className="flex justify-between">
-        <div className="w-[425px] h-[350px] bg-[var(--primary-color)]">
-
+      <div className="flex flex-col gap-5">
+        <div className="text-5xl text-[var(--primary-color)] font-extrabold">
+            <h1>Your Groups</h1>
         </div>
-        <div className="w-[425px] h-[350px] bg-[var(--primary-color)]">
-
+        <div className="border border-[var(--primary-color)] rounded-[10px] p-8">
+          <MovieGroups/>
+        </div>
+      </div>
+      <div className="grid grid-cols-2 gap-24 pt-8">
+        <div className="flex flex-col gap-5">
+          <div className="text-5xl text-[var(--primary-color)] font-extrabold">
+              <h1>Create a Group</h1>
+          </div>
+          <CreateGroup/>
+        </div>
+        <div className="flex flex-col gap-5">
+          <div className="text-5xl text-[var(--primary-color)] font-extrabold">
+              <h1>Join a Group</h1>
+          </div>
+          <JoinGroup/>
         </div>
       </div>
     </div>
