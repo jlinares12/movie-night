6a36efc5f43a473e8db07798c262d685ab8635f2
Juan Linares <juanlinares821@csu.fullerton.edu>
2025-07-11 23:59:47 -0700

    Added functinality so that if the data we are getting from the backend is loading or empty we show a skeleton component for the groups link

--- src/components/MovieGroups.tsx
+++ src/components/MovieGroups.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import GroupLink from "./GroupLink";
+import GroupLinkSkeleton from "./skeletons/GroupLinkSkeleton";
 
 export default function MovieGroups() {
     const [data, setData] = useState([])
@@ -16,8 +17,11 @@ export default function MovieGroups() {
     return (
         <div className="flex flex-col items-center justify-center">
             <div className="w-full justify-self-start">
-                {(typeof data === 'undefined') ? (
-                <p>Loading</p>
+                {(data.length === 0) ? (
+                    <div className="divide-y divide-[var(--primary-gray)]">
+                        <GroupLinkSkeleton/>
+                        <GroupLinkSkeleton/>
+                    </div>
                 ): (
                 <ul className="list-none divide-y divide-[var(--primary-gray)]">
                     {data.map((group, i) => (
