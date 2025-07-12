a76a86875a54be55c85b9609103fed1628822c7b
Juan Linares <juanlinares821@csu.fullerton.edu>
2025-07-11 23:58:50 -0700

    Added conditional classnames and disabled feature to show if it is in skeleton mode or not

--- src/components/buttons/DangerButton.tsx
+++ src/components/buttons/DangerButton.tsx
@@ -1,10 +1,14 @@
 interface Props {
     label: string;
+    isDisabled?: boolean;
 }
 
-export default function DangerButton({label}: Props) {
+export default function DangerButton({label, isDisabled}: Props) {
     return (
-        <button className="bg-red-500 rounded-[10px] text-[var(--text-color)] text-sm font-extrabold p-3 min-w-[75px]">
+        <button className={`rounded-[10px] text-sm font-extrabold p-3 min-w-[75px] ${
+            isDisabled ? "text-[var(--light-gray)] animate-pulse" : "bg-red-500 text-[var(--text-color)]"
+        }`} disabled = {isDisabled}
+        >
             {label}
         </button>
     )
