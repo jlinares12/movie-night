08ed8113a9c002426cfe14f38333e69e30c1016f
Juan Linares <juanlinares821@csu.fullerton.edu>
2025-07-11 23:58:50 -0700

    Added conditional classnames and disabled feature to show if it is in skeleton mode or not

--- src/components/buttons/OutlinedButton.tsx
+++ src/components/buttons/OutlinedButton.tsx
@@ -1,10 +1,14 @@
 interface Props {
     label: string;
+    isDisabled?: boolean;
 }
 
-export default function OutlinedButton({label}: Props) {
+export default function OutlinedButton({label, isDisabled}: Props) {
     return (
-        <button className="border border-[var(--primary-color)] rounded-[10px] text-[var(--primary-color)] text-sm font-extrabold p-3 min-w-[75px]">
+        <button className={`border rounded-[10px] text-sm font-extrabold p-3 min-w-[75px] ${
+            isDisabled ? "border-[var(--primary-gray)] text-[var(--light-gray)] animate-pulse" : "border-[var(--primary-color)] text-[var(--text-color)]"
+        }`} disabled = {isDisabled}
+        >
             {label}
         </button>
     )
