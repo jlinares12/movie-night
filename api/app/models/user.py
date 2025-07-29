34628c9bb9b0304d53c6831e7aae9c3ea6831eae
Juan Linares <juanlinares821@csu.fullerton.edu>
2025-07-29 15:07:23 -0700

    Updated the user model to have a string userID instead of an integer

--- api/app/models/user.py
+++ api/app/models/user.py
@@ -2,5 +2,5 @@ from app.extensions import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, index=True, unique=True)
+    user_id = db.Column(db.String(64), index=True, unique=True)
     username = db.Column(db.String(64), index=True, unique=True)
