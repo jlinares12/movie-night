ae8cd791c110d87559b63d583f795a19bfbd9607
Juan Linares <juanlinares821@csu.fullerton.edu>
2025-07-29 15:09:29 -0700

    Updated the app routes to include the webhooks

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -17,8 +17,10 @@ def create_app(config_class=Config):
     # Register blueprints
     from app.routes.groups import bp as groups_bp
     from app.routes.tech_stack import bp as tech_bp
+    from app.routes.webhooks import bp as webhook_bp
 
     app.register_blueprint(groups_bp)
     app.register_blueprint(tech_bp)
+    app.register_blueprint(webhook_bp)
 
     return app
