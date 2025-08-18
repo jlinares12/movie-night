7832213050730a176eada4b3328fd50e43de1518
Juan Linares <juanlinares821@csu.fullerton.edu>
2025-08-18 16:45:29 -0700

    Updated the movie groups component to keep requesting for data until it is fullfilled

--- src/components/MovieGroups.tsx
+++ src/components/MovieGroups.tsx
@@ -1,28 +1,47 @@
 import { useState, useEffect } from "react";
-import axios from "axios";
-import GroupLink from "./GroupLink";
 import GroupLinkSkeleton from "./skeletons/GroupLinkSkeleton";
+import GroupLink from "./GroupLink";
+import axios from "axios";
+
+interface Group {
+  name: string;
+  user_count: number;
+  date: string;
+}
 
 export default function MovieGroups() {
-    const [data, setData] = useState([])
-    const fetchAPI = async () => {
-    const response = await axios.get("api/groups");
-    setData(response.data);
-  }
+  const [data, setData] = useState<Group[]>([]);
+  const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    fetchAPI();
-  },[])
+useEffect(() => {
+  let intervalId: number;
+
+  const fetchAPI = async () => {
+    try {
+        const response = await axios.get<Group[]>("api/groups");
+        setData(response.data);
+        setLoading(false);
+        if (response.data.length >= 0) {
+            clearInterval(intervalId);
+        }
+    } catch (err: any) {
+        setLoading(true);
+    }
+  };
+
+  fetchAPI();
+  intervalId = window.setInterval(fetchAPI, 2000);
+}, []);
 
     return (
         <div className="flex flex-col items-center justify-center">
             <div className="w-full justify-self-start">
-                {(data.length === 0) ? (
+                {loading ? (
                     <div className="divide-y divide-[var(--primary-gray)]">
                         <GroupLinkSkeleton/>
                         <GroupLinkSkeleton/>
                     </div>
-                ): (
+                ) : (
                 <ul className="list-none divide-y divide-[var(--primary-gray)]">
                     {data.map((group, i) => (
                         <GroupLink
