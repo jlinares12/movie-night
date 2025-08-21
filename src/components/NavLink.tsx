788d4021e7dccfd7b9a19eb85202fd5384268fee
Juan Linares <juanlinares821@csu.fullerton.edu>
2025-08-21 09:30:33 -0700

    Changed the dimensions of the component

--- src/components/NavLink.tsx
+++ src/components/NavLink.tsx
@@ -8,7 +8,7 @@ interface Props {
 export default function NavLink({icon, label}:Props) {
     return (
         <div className="flex items-center gap-[15px]">
-            <div className="w-[30px] h-[30px]">
+            <div className="w-[20px] h-[20px]">
             {icon("w-full h-full")}
             </div>
             {label}
