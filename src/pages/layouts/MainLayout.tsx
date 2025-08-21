1ea86ffa98a43b7a61490a412a7e95369f8b9a12
Juan Linares <juanlinares821@csu.fullerton.edu>
2025-08-21 09:29:52 -0700

    Removed the sidebar, which will get replaced with a different component

--- src/pages/layouts/MainLayout.tsx
+++ src/pages/layouts/MainLayout.tsx
@@ -1,13 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Header from "../../components/Header";
-import SideBar from "../../components/SideBar";
 
 export default function MainLayout() {
     return (
         <div className="h-screen bg-[var(--bk-color)] text-white flex flex-col">
             <Header/>
             <div className="flex h-full">
-                <SideBar/>
                 <div className="w-full fixed flex flex-col items-center pt-8 overflow-scroll">
                     <Outlet/>
                 </div>
