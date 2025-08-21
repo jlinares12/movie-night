47430282461ccd30e1f13dd924c02e821c744dd5
Juan Linares <juanlinares821@csu.fullerton.edu>
2025-08-21 09:30:16 -0700

    Added the profile and logout links to the header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,15 @@
-import { useUser } from "@clerk/clerk-react"
+import { useClerk, useUser } from "@clerk/clerk-react"
 import { Link } from "react-router-dom";
 import MovieClapper from "./icons/MovieClapper"
 import NotificationIcon from "./icons/NotificationIcon"
+import ProfileIcon from "./icons/ProfileIcon";
+import LogoutIcon from "./icons/LogoutIcon";
 import NavLink from "./NavLink";
 
 export default function Header() {
-    const {user} = useUser();
+    const { user } = useUser();
+    const { signOut } = useClerk()
+
     return (
         <header className="h-[65px] flex items-center grid grid-cols-3 pr-8 pl-8">
             <div className="justify-self-start">
@@ -19,8 +23,20 @@ export default function Header() {
             <div className="justify-self-center text-[var(--text-color)]">
                 <h1>{user?.username}</h1>
             </div>
-            <div className="flex w-[30px] h-[30px] justify-self-end">
-                <NotificationIcon color="#40D952" className="w-full h-full"/>
+            <div className="grid grid-cols-3 justify-self-end gap-4">
+                    <NotificationIcon color="#40D952" className="w-[20px] h-[20px]"/>
+                    <Link to={"/profile"}>
+                        <NavLink
+                            icon={(className) => <ProfileIcon color="#40D952" className={className}/>}
+                            label=""
+                        />
+                    </Link>
+                    <button onClick={() => signOut({ redirectUrl: '/login' })}>
+                        <NavLink
+                            icon={(className) => <LogoutIcon color="#d60303ff" className={className}/>}
+                            label=""
+                        />
+                    </button>
             </div>
         </header>
     )
