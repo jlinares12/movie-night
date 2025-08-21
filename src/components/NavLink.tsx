d890e041758ca9ea461474c2c7ae9de648f46b0a
Juan Linares <juanlinares821@csu.fullerton.edu>
2025-08-21 09:32:24 -0700

    Updated the gap between icon and label

--- src/components/NavLink.tsx
+++ src/components/NavLink.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 export default function NavLink({icon, label}:Props) {
     return (
-        <div className="flex items-center gap-[15px]">
+        <div className="flex items-center gap-4">
             <div className="w-[20px] h-[20px]">
             {icon("w-full h-full")}
             </div>
