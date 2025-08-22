6e095246751fc41846e577bd22da2cfe5474dbb0
Juan Linares <juanlinares821@csu.fullerton.edu>
2025-08-21 21:32:43 -0700

    Created two components that will be part of the group layout

---
+++ src/components/RightSidebar.tsx
@@ -0,0 +1,21 @@
+export default function RightSidebar() {
+    return (
+        <div className="flex flex-col">
+            <div className="info">
+                <div>
+                    <h1>Our Dev's Faves</h1>
+                </div>
+                <div>
+                    <h2>Movies</h2>
+                </div>
+                <div>
+                    <h2>Snacks</h2>
+                </div>
+            </div>
+            <div className="flex flex-col">
+                <span>Report a bug</span>
+                <span>Submit user feedback</span>
+            </div>
+        </div>
+    )
+}
