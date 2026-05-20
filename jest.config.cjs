7f5c82f3215357d9314dac17bb6fa5ab06a80943
Juan Linares <juanlinares821@yahoo.com>
2026-05-20 15:23:04 -0700

    added: config files for Jest

---
+++ jest.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', { tsconfig: 'tsconfig.jest.json' }],
+  },
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '^@mui/icons-material$': '<rootDir>/src/__mocks__/muiIconsMock.tsx',
+  },
+};
