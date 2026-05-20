4dcee1752fa2d758b13a0db2461bae1aac8c376e
Juan Linares <juanlinares821@yahoo.com>
2026-05-20 02:28:27 -0700

    fix: added SQLAlchemy engine options for pooled requests

--- api/config.py
+++ api/config.py
@@ -4,3 +4,9 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ENGINE_OPTIONS = {
+        "pool_pre_ping": True,
+        "pool_recycle": 300,
+        "pool_size": 10,
+        "max_overflow": 20,
+    }
