52dcd1e6072c82b4f3b9b3622419daceca6acce9
Juan Linares <juanlinares821@yahoo.com>
2026-05-23 17:53:54 -0700

    added onClick function to button props

--- src/components/buttons/DangerButton.tsx
+++ src/components/buttons/DangerButton.tsx
@@ -1,13 +1,17 @@
 interface Props {
     label: string;
     isDisabled?: boolean;
+    onClick?: () => void;
 }
 
-export default function DangerButton({label, isDisabled}: Props) {
+export default function DangerButton({label, isDisabled, onClick}: Props) {
     return (
-        <button className={`rounded-[10px] text-sm font-extrabold p-3 min-w-[75px] ${
-            isDisabled ? "text-[var(--light-gray)] animate-pulse" : "bg-red-500 text-[var(--text-color)]"
-        }`} disabled = {isDisabled}
+        <button
+            className={`rounded-[10px] text-sm font-extrabold p-3 min-w-[75px] ${
+                isDisabled ? "text-[var(--light-gray)] animate-pulse" : "bg-red-500 text-[var(--text-color)]"
+            }`}
+            disabled={isDisabled}
+            onClick={onClick}
         >
             {label}
         </button>
