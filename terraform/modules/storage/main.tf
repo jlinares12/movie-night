d1e7f5130a19e6218d68885ce4edc9991d54f63d
Juan Linares <juanlinares821@yahoo.com>
2026-06-08 15:11:28 -0700

    fix: added project id to both buckets and artifact registry

--- terraform/modules/storage/main.tf
+++ terraform/modules/storage/main.tf
@@ -16,6 +16,7 @@ variable "environment" {
 
 resource "google_storage_bucket" "gcs_state_bucket" {
     name     = "${var.project_id}-${var.environment}-tfstate"
+    project  = var.project_id
     location = var.region
     versioning {
       enabled = true
@@ -25,6 +26,7 @@ resource "google_storage_bucket" "gcs_state_bucket" {
 resource "google_storage_bucket" "frontend_storage_bucket" {
     name     = "${var.project_id}-${var.environment}-frontend-bucket"
     location = var.region
+    project  = var.project_id
     website {
       main_page_suffix = "index.html"
       not_found_page   = "index.html"
@@ -33,6 +35,7 @@ resource "google_storage_bucket" "frontend_storage_bucket" {
 
 resource "google_artifact_registry_repository" "docker-repo" {
     location      = var.region
+    project  = var.project_id
     repository_id = "${var.project_id}-${var.environment}-artifact-registry"
     description   = "example docker repository"
     format        = "DOCKER"
