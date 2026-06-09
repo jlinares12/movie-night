3589e594d5dbf2c9ce3cdbee1f3b82046727b158
Juan Linares <juanlinares821@yahoo.com>
2026-06-08 18:12:02 -0700

    fix: extracted variables to their own file and added versions to include the random block

--- terraform/modules/sql/main.tf
+++ terraform/modules/sql/main.tf
@@ -1,35 +1,3 @@
-variable "project_id" {
-    description = "GCP project ID"
-    type        = string
-}
-
-variable "region" {
-    description = "Where to deploy resources"
-    type        = string
-    default     = "us-central1"
-}
-
-variable "db_name" {
-    description = "Name of the PostgreSQL database"
-    type        = string
-}
-
-variable "db_user" {
-    description = "Name of the PostgreSQL app user"
-    type        = string
-}
-
-variable "db_password" {
-    description = "The Cloud SQL app user password"
-    type        = string
-    sensitive   = true
-}
-
-variable "deletion_protection" {
-    description = "States if we want to enable deletion protection"
-    type        = bool
-}
-
 resource "random_id" "call_time_db_id" {
   byte_length = 4
 }
