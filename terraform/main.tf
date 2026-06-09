93634dd58c7bd6532da5d4b71ac161a571f68bd0
Juan Linares <juanlinares821@yahoo.com>
2026-06-08 18:10:34 -0700

    fix: added backend block

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,14 @@
 terraform {
+    required_version = "~> 1.9"
+
     required_providers {
       google = {
-        source = "hashicorp/google"
+        source  = "hashicorp/google"
         version = "~> 5.0"
       }
     }
+
+    backend "gcs" {}
 }
 
 provider "google" {
