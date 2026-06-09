61c977f20f815ed39bb9fe3305d3806fe5d29b7d
Juan Linares <juanlinares821@yahoo.com>
2026-06-08 18:12:47 -0700

    fix: moved variables to their own file

---
+++ terraform/modules/storage/variables.tf
@@ -0,0 +1,15 @@
+variable "project_id" {
+    description = "GCP project ID"
+    type        = string
+}
+
+variable "region" {
+    description = "Where to deploy resources"
+    type        = string
+    default     = "us-central1"
+}
+
+variable "environment" {
+    description = "Name resources and toggle behaviors (dev or prod)"
+    type        = string
+}
