3c2bf61f7401826916cac4e0d6bf3ff946703eab
Juan Linares <juanlinares821@yahoo.com>
2026-06-12 11:48:53 -0700

    add: declared variables used by module

---
+++ terraform/modules/iam/variables.tf
@@ -0,0 +1,14 @@
+variable "project_id" {
+  description = "GCP project ID"
+  type = string
+}
+
+variable "environment" {
+  description = "Env or Prod environments"
+  type = string
+}
+
+variable "frontend_bucket_name" {
+  description = "Name of the GCS bucket for the frontend"
+  type = string
+}
