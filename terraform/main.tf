1cff85b6962d4446a1ca840d933e3cb261440825
Juan Linares <juanlinares821@yahoo.com>
2026-06-12 13:47:11 -0700

    fix: enabled GCP APIs via terraform and added depends_on to each module to wait for verification of APIs being enabled

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,12 +16,51 @@ provider "google" {
     region  = var.region
 }
 
+# API Registry
+resource "google_project_service" "compute" {
+  project            = var.project_id
+  service            = "compute.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "artifact-registry" {
+  project            = var.project_id
+  service            = "artifactregistry.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "iam" {
+  project            = var.project_id
+  service            = "iam.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "secret-manager" {
+  project            = var.project_id
+  service            = "secretmanager.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "google-run" {
+  project            = var.project_id
+  service            = "run.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "sql" {
+  project            = var.project_id
+  service            = "sqladmin.googleapis.com"
+  disable_on_destroy = false
+}
+
+# Modules
 module "storage" {
     source = "./modules/storage"
 
     project_id  = var.project_id
     environment = var.environment
     region      = var.region
+    depends_on  = [ google_project_service.artifact-registry ]
 }
 
 module "sql" {
@@ -33,6 +72,7 @@ module "sql" {
     db_password         = var.db_password
     db_name             = var.db_name
     db_user             = var.db_user
+    depends_on          = [ google_project_service.sql ]
 }
 
 module "iam" {
@@ -43,6 +83,7 @@ module "iam" {
   frontend_bucket_name = module.storage.frontend_bucket_name
   project_number = var.project_number
   github_repo = var.github_repo
+  depends_on = [ google_project_service.iam ]
 }
 
 module "networking" {
@@ -50,4 +91,5 @@ module "networking" {
 
   project_id  = var.project_id
   environment = var.environment
+  depends_on  = [ google_project_service.compute ]
 }
