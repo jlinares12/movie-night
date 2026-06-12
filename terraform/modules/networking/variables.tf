1cc9ad9abc9c316f57a2c8ed4be93e8c0a55cac9
Juan Linares <juanlinares821@yahoo.com>
2026-06-12 13:06:32 -0700

    add: created networking module that holds a static external global IP

---
+++ terraform/modules/networking/variables.tf
@@ -0,0 +1,9 @@
+variable "environment" {
+  description = "The environment we are in (prod or dev)"
+  type = string
+}
+
+variable "project_id" {
+  description = "GCP project ID"
+  type = string
+}
