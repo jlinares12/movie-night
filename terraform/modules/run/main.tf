2383e57c6791bcb2b640e652b685889a98b59e25
Juan Linares <juanlinares821@yahoo.com>
2026-06-26 00:19:52 -0700

    fix: added volumes to run service and fixed iam member resource's format

--- terraform/modules/run/main.tf
+++ terraform/modules/run/main.tf
@@ -3,14 +3,15 @@ resource "google_cloud_run_v2_service" "call-time" {
   location = var.region
   ingress  = "INGRESS_TRAFFIC_INTERNAL_LOAD_BALANCER"
   template {
-    service_account = var.service-account-email
-    annotations     = {
-      "run.googleapis.com/cloudsql-instances" = var.db-instance-connection-name
-    }
+    service_account = var.service_account_email
     containers {
       image = "us-docker.pkg.dev/cloudrun/container/hello"
       ports {
         container_port = 8080
+      }
+      volume_mounts {
+        name       = "cloudsql"
+        mount_path = "/cloudsql"
       }
       dynamic "env" {
         for_each = var.secret_ids
@@ -25,6 +26,12 @@ resource "google_cloud_run_v2_service" "call-time" {
         }
       }
     }
+    volumes {
+      name = "cloudsql"
+      cloud_sql_instance {
+        instances = [ var.db_instance_connection_name ]
+      }
+    }
   }
   lifecycle {
     ignore_changes = [ template[0].containers[0].image ]
@@ -32,8 +39,8 @@ resource "google_cloud_run_v2_service" "call-time" {
 }
 
 resource "google_cloud_run_v2_service_iam_member" "cloud_run_service_member" {
-  member = "allUsers"
-  name = google_cloud_run_v2_service.call-time.name
-  role = "roles/run.invoker"
-  location = var.region
+  member    = "allUsers"
+  name      = google_cloud_run_v2_service.call-time.name
+  role      = "roles/run.invoker"
+  location  = var.region
 }
