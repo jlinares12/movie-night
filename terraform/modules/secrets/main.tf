21387cd215aa8df9ba88d1df9208b10b95c38aaa
Juan Linares <juanlinares821@yahoo.com>
2026-06-26 00:22:32 -0700

    fix: spacing conventions

--- terraform/modules/secrets/main.tf
+++ terraform/modules/secrets/main.tf
@@ -1,6 +1,6 @@
 resource "google_secret_manager_secret" "call-time-secrets" {
-  for_each = toset(var.secret_names)
-  project = var.project_id
+  for_each  = toset(var.secret_names)
+  project   = var.project_id
   secret_id = "call-time-${each.key}"
   replication {
     auto {}
