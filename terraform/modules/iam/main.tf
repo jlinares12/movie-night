a68543fb1acd782d1bc6e155adbea6b04bd638cc
Juan Linares <juanlinares821@yahoo.com>
2026-07-02 21:09:09 -0700

    removed github service accoutn and WIF resources

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -1,9 +1,4 @@
 # Creates Service Accounts
-resource "google_service_account" "github-actions-sa" {
-  display_name = "${var.project_id}-${var.environment}-github-actions"
-  account_id   = "${var.environment}-github-actions-sa"
-  project      = var.project_id
-}
 
 resource "google_service_account" "cloud-run-sa" {
   display_name = "${var.project_id}-${var.environment}-cloud-run"
@@ -57,30 +52,4 @@ resource "google_storage_bucket_iam_member" "github-actions-storage-policy" {
   member = "serviceAccount:${google_service_account.github-actions-sa.email}"
   role   = "roles/storage.objectAdmin"
   bucket = var.frontend_bucket_name
-}
-
-# WIF Resources
-resource "google_iam_workload_identity_pool" "github" {
-  workload_identity_pool_id = "${var.environment}-iam-pool"
-  project                   = var.project_id
-}
-
-resource "google_iam_workload_identity_pool_provider" "github" {
-  workload_identity_pool_provider_id = "${var.environment}-pool-provider"
-  workload_identity_pool_id          = google_iam_workload_identity_pool.github.workload_identity_pool_id
-  attribute_condition                = "assertion.repository == \"${var.github_repo}\""
-
-  oidc {
-    issuer_uri = "https://token.actions.githubusercontent.com"
-  }
-  attribute_mapping = {
-    "google.subject"       = "assertion.sub"
-    "attribute.repository" = "assertion.repository"
-  }
-}
-
-resource "google_service_account_iam_member" "wif-binding" {
-  service_account_id = google_service_account.github-actions-sa.name
-  role               = "roles/iam.workloadIdentityUser"
-  member             = "principalSet://iam.googleapis.com/projects/${var.project_number}/locations/global/workloadIdentityPools/${google_iam_workload_identity_pool.github.workload_identity_pool_id}/attribute.repository/${var.github_repo}"
 }
