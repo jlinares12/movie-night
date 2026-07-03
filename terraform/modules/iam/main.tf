c3ab30a71c5247b56904934f9dcf33d0f73fa4fa
Juan Linares <juanlinares821@yahoo.com>
2026-07-02 23:14:13 -0700

    wired up all iam policies needed for cloud build

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -6,6 +6,12 @@ resource "google_service_account" "cloud-run-sa" {
   project      = var.project_id
 }
 
+resource "google_service_account" "cloudbuild-sa" {
+  display_name = "${var.project_id}-${var.environment}-cloudbuild"
+  account_id   = "${var.environment}-cloudbuild-sa"
+  project      = var.project_id
+}
+
 # IAM Member Project Policies
 
 # Policies for Cloud Run
@@ -27,4 +33,39 @@ resource "google_project_iam_member" "cloud-run-artifact-policy" {
   role    = "roles/artifactregistry.reader"
 }
 
+# Cloudbuild IAM Policies
 
+## Deploy new revisions
+resource "google_project_iam_member" "cloudbuild-run-policy" {
+  project = var.project_id
+  member  = "serviceAccount:${google_service_account.cloudbuild-sa.email}"
+  role    = "roles/run.admin"
+}
+
+## Act as the Cloud Run runtime SA when deploying
+resource "google_service_account_iam_member" "cloudbuild-actas-runtime" {
+  service_account_id = google_service_account.cloud-run-sa.id
+  role = "roles/iam.serviceAccountUser"
+  member = "serviceAccount:${google_service_account.cloudbuild-sa.email}"
+}
+
+resource "google_artifact_registry_repository_iam_member" "cloudbuild-artifact-policy" {
+  project    = var.project_id
+  location   = var.region
+  repository = var.artifact_registry_repository_id
+  member     = "serviceAccount:${google_service_account.cloudbuild-sa.email}"
+  role       = "roles/artifactregistry.writer"
+}
+
+## Cloud Build needs to sync to frontend bucket
+resource "google_storage_bucket_iam_member" "cloudbuild-storage-policy" {
+  member = "serviceAccount:${google_service_account.cloudbuild-sa.email}"
+  role   = "roles/storage.objectAdmin"
+  bucket = var.frontend_bucket_name
+}
+
+resource "google_project_iam_member" "cloudbuild-logs-policy" {
+  project = var.project_id
+  member  = "serviceAccount:${google_service_account.cloudbuild-sa.email}"
+  role    = "roles/logging.logWriter"
+}
