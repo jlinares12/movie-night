caf2c40393366399bb71f8338eb2a397aa0079ad
Juan Linares <juanlinares821@yahoo.com>
2026-07-07 13:29:19 -0700

    changed the variable imported from iam module from email to id of the cloudbuild service account

--- terraform/modules/iam/outputs.tf
+++ terraform/modules/iam/outputs.tf
@@ -2,6 +2,6 @@ output "sql-service-account-email" {
   value = google_service_account.cloud-run-sa.email
 }
 
-output "cloud-build-service-account-email" {
-  value = google_service_account.cloudbuild-sa.email
+output "cloud-build-service-account-id" {
+  value = google_service_account.cloudbuild-sa.id
 }
