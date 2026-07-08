c01af933885dc53ed4ac61940eb30d6b5d329e15
Juan Linares <juanlinares821@yahoo.com>
2026-07-07 20:15:45 -0700

    created env secrets for each cloud run instance

--- terraform/modules/secrets/main.tf
+++ terraform/modules/secrets/main.tf
@@ -1,7 +1,7 @@
 resource "google_secret_manager_secret" "call-time-secrets" {
   for_each  = toset(var.secret_names)
   project   = var.project_id
-  secret_id = "call-time-${each.key}"
+  secret_id = "call-time-${var.environment}-${each.key}"
   replication {
     auto {}
   }
