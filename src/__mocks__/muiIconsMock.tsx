c39dfdd67e5fc360806240ab5549c6688b1ca20c
Juan Linares <juanlinares821@yahoo.com>
2026-08-22 08:46:22 -0700

    removed unused mock items

--- src/__mocks__/muiIconsMock.tsx
+++ src/__mocks__/muiIconsMock.tsx
@@ -1,8 +1,5 @@
 const MockIcon = () => <span data-testid="mock-icon" />;
 
 export const Movie = MockIcon;
-export const Notifications = MockIcon;
-export const AccountCircle = MockIcon;
-export const Logout = MockIcon;
 export const SearchOff = MockIcon;
 export default MockIcon;
