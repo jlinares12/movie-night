65b901069176d858818a26b988fa0d462bd2a9e8
Juan Linares <juanlinares821@yahoo.com>
2026-09-04 12:45:08 -0700

    added a scaling resaurce to my cloud run template. if the environment is prod I set the min instance count to 1 if it isn't i set it to 0

--- terraform/modules/run/main.tf
+++ terraform/modules/run/main.tf
@@ -4,6 +4,9 @@ resource "google_cloud_run_v2_service" "call-time" {
   ingress  = "INGRESS_TRAFFIC_INTERNAL_LOAD_BALANCER"
   template {
     service_account = var.service_account_email
+    scaling {
+      min_instance_count = (var.environment == "prod") ? 1 : 0
+    }
     containers {
       image = "us-docker.pkg.dev/cloudrun/container/hello"
       ports {
